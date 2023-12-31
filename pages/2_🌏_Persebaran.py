a629660cbb9badcca138e0f367c34fddff21195e
IRSYAD FADILLAH <116770290+IrsyadFadillah@users.noreply.github.com>
2024-01-01 02:12:09 +0700

    modified:   "pages/2_\360\237\214\217_Persebaran.py"

--- pages/2_🌏_Persebaran.py
+++ pages/2_🌏_Persebaran.py
@@ -52,16 +52,58 @@ def main():
 
     # Menambahkan artikel atau deskripsi panjang
     st.write("""
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod elit nec sapien
-        laoreet, vel cursus leo ultrices. Sed a justo vel quam facilisis feugiat. Proin vel elit
-        vitae velit aliquet efficitur. Mauris dignissim nisi ut nisl efficitur, et venenatis nulla
-        lacinia. Nullam bibendum eros eget elit sagittis, vitae auctor elit feugiat.
-
-        Duis sit amet semper est. Fusce aliquet urna eu ligula auctor fringilla. Integer fermentum
-        risus vel quam fermentum, ac blandit dolor tincidunt. Curabitur id purus eget urna
-        vestibulum cursus. Mauris non metus quis risus dictum mattis. Fusce euismod elit eget
-        justo tincidunt, ut dapibus ex tempor. Vestibulum tincidunt augue eu libero interdum
-        sollicitudin.
+        Sejak dikeluarkannya Peraturan Pemerintah No. 60 Tahun 2007 tentang Konservasi Sumber
+        Daya Ikan dan Undang-Undang No. 27 Tahun 2007 tentang Pengelolaan Wilayah Pesisir dan
+        Pulau-pulau Kecil, sejumlah Kawasan Konservasi Perairan (KKP) dan Kawasan Konservasi
+        Pesisir dan Pulau-Pulau Kecil (KKP3K), telah didirikan hampir di seluruh wilayah Indonesia.
+        Hingga tahun 2013 tercatat sebanyak 99 KKP/KKP3K telah dibentuk dengan luas total
+        11.069.263 ha. KKP/KKP3K yang dibentuk tersebut meliputi Kawasan Konservasi Perairan
+        Nasional (KKPN), Kawasan Konservasi Perarain Daerah (KKPD) / Kawasan Konservasi
+        Pesisir dan Pulau-Pulau Kecil Daerah (KKP3KD) dengan berbagai kategori. Jika ditambah
+        dengan kawasan konservasi yang pendiriannya mengacu pada UU No. 5 Tahun 1990 tentang
+        Konservasi Sumber Daya Hayati dan Ekosistemnya (sebanyak 32 KKP dengan total luas
+        4.694.947 ha), maka secara keseluruhan telah didirikan 131 KKP di Indonesia dengan
+        total luas 15.764.211 ha.
+             
+        KKP didefinisikan sebagai kawasan perairan yang dilindungi, dikelola dengan sistem zonasi,
+        untuk mewujudkan pengelolaan sumber daya ikan dan lingkungannya secara berkelanjutan
+        (PP 60/2007). Secara umum KKP didefinisikan sebagai kawasan yang diperuntukkan dan dikelola
+        baik secara formal maupun tidak formal agar dalam jangka panjang untuk dapat melindungi
+        sumberdaya alam berikut jasa-jasa ekosistem dan nilai-nilai budayanya (IUCN-WCPA, 2008).
+        Untuk mewujudkan tujuan pembentukan KKP tersebut, maka diperlukan upaya pengelolaan
+        secara efektif. KKP dapat dikelola dengan lebih efektif dengan cara berjejaring. Jejaring yang dirancang
+        dengan baik memberikan hubungan spasial penting yang diperlukan untuk memelihara proses-proses
+        ekosistem dan keterkaitannya, serta meningkatkan kelentingan (resilience) dengan cara 
+        memperkecil resiko jika terjadi bencana-bencana lokal, perubahan iklim, kegagalan pengelolaan
+        atau masalah lain. Dengan demikian, kawasan konservasi yang berjejaring akan membantu
+        menjamin kelestarian populasi jangka panjang secara lebih baik jika dibandingkan satu kawasan
+        konservasi saja (NRC, 2002).
+             
+        Sebagian dari KKP di Indonesia memiliki keterkaitan antara satu dengan lainnya. Faktor
+        keterkaitan tersebut disebabkan adanya hubungan secara biofisik, sosial-budaya-ekonomi dan/
+        atau tata kelola. Keterkaitan satu KKP dengan KKP lainnya dapat mempengaruhi keberhasilan
+        pengelolaan kawasan konservasi yang saling terkait tersebut. Sebagai contoh suatu KKP yang
+        mengalami kerusakan terumbu karang dapat dibantu pemulihannya oleh KKP lainnya yang
+        kondisi terumbu karangnya masih terjaga, melalui arus laut yang menghubungkan kedua kawasan
+        tersebut. Saat ini diberbagai tempat di Indonesia tengah diinisiasi pembentukan jejaring KKP. Untuk
+        mendapatkan gambaran serta memahami bagaimana proses dan aspek keterkaitan dalam
+        pembentukan jejaring-jejaring KKP tersebut, maka disusun profil jejaring KKP.
+             
+        Kategori KKP menurut Permen No. 17 Tahun 2008 terdiri atas Kawasan Konservasi Pesisir &
+        Pulau Pulau Kecil (KKP3K), Kawasan Konservasi Maritim (KKM), dan Kawasan Konservasi
+        Perairan (KKP). Jika dirinci, KKP3K terdiri dari Suaka Pesisir, Suaka Pulau Kecil, Taman Pesisir,
+        dan Taman Pulau Kecil. KKM terdiri dari Perlindungan Adat Maritim dan Perlindungan Budaya
+        Maritim. KKP terdiri dari Taman Nasional Perairan, Taman Wisata Perairan, Suaka Alam Perairan,
+        dan Suaka Perikanan. Upaya pengembangan KKP di berbagai negara dilandasi oleh kemanfaatan KKP yang mencakup
+        multi aspek. Roberts & Hawkins (2000) menyusun daftar manfaat KKP, yaitu (i) melindungi
+        eksploitasi populasi dan memperbaiki produksi benih yang akan membantu restoking untuk
+        daerah penangkapan ikan, (ii) mendukung usaha perikanan, yaitu dengan adanya spillover ikan
+        dewasa dan juvenil ke daerah penangkapan ikan, (iii) menyediakan perlindungan terhadap
+        spesies-spesies yang sensitif terhadap kegiatan penangkapan, (iv) mencegah kerusakan habitat
+        dan membantu tahap pemulihan habitat, (v) memelihara keanekaragaman dengan cara
+        membantu pengembangan komunitas biologi alami yang berbeda dengan yang ada di daerah
+        penangkapan, serta (vi) membantu pemulihan ekosistem yang rusak oleh gangguan dari manusia
+        dan alam.
 
         ... (Tambahkan artikel panjang sesuai kebutuhan Anda)
     """)
